c81f4f92c6eabab1de4779611175b64c1da951ac
Victor Fernando Ferreira Gomes <victorgbpk@gmail.com>
2022-06-15 10:37:03 -0300

    Login routes and components form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
 
 function App() {
   return (
     <div>
-      App React
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login/*' element={<Login />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
