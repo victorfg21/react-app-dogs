7f637cd19436a96835b5943bdca36c005b27181e
Victor Fernando Ferreira Gomes <victorgbpk@gmail.com>
2022-06-27 08:26:07 -0300

    Login e cadastro

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -6,6 +6,10 @@ const types = {
 			/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
 		message: "Preencha um email válido",
 	},
+	password:{
+		regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+		message: 'A senha precisa ter 1 carácter maiúsculo, 1 minúsculo e 1 dígito. Com no mínimo 8 caracteres.'
+	}
 };
 
 function useForm(type) {
