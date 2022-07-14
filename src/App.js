8fe355869eb034959e4fd27f0e838b8b9dcc6cf7
Victor Fernando Ferreira Gomes <victorgbpk@gmail.com>
2022-07-14 14:25:07 -0300

    User profile, not found page and propTypes example

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import ProtectedRoute from "./Components/Helper/ProtectedRoute";
 import Home from "./Components/Home";
 import Login from "./Components/Login/Login";
 import Photo from "./Components/Photo/Photo";
+import NotFound from "./Components/User/NotFound";
 import User from "./Components/User/User";
+import UserProfile from "./Components/User/UserProfile";
 import { UserStorage } from "./UserContext";
 
 function App() {
@@ -26,6 +28,8 @@ function App() {
 					}
 				/>
 				<Route path="foto/:id" element={<Photo />} />
+				<Route path="perfil/:user" element={<UserProfile />} />
+				<Route path="*" element={<NotFound />} />
 			</Routes>
 			<Footer />
 		</UserStorage>
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -6,6 +6,7 @@ import LoginForm from "./LoginForm";
 import LoginPasswordLost from "./LoginPasswordLost";
 import LoginPasswordReset from "./LoginPasswordReset";
 import styles from "./Login.module.css";
+import NotFound from "../User/NotFound";
 
 const Login = () => {
 	const { login } = useContext(UserContext);
@@ -20,6 +21,7 @@ const Login = () => {
 					<Route path="criar" element={<LoginCreate />} />
 					<Route path="perdeu" element={<LoginPasswordLost />} />
 					<Route path="resetar" element={<LoginPasswordReset />} />
+					<Route path="*" element={<NotFound />} />
 				</Routes>
 			</div>
 		</section>
